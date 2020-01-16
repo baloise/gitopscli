99d1f10a1695e38c61f24203e5c9891c33e80698
Christian Siegel <christian.siegel@gmx.net>
2020-01-16 20:43:14 +0100

    add an argument parser

--- gitopscli/__main__.py
+++ gitopscli/__main__.py
@@ -1,12 +1,27 @@
+import argparse
 import sys
+import yaml
 
 
 def main():
-    print("in main")
-    args = sys.argv[1:]
-    print("count of args :: {}".format(len(args)))
-    for arg in args:
-        print("passed argument :: {}".format(arg))
+    parser = argparse.ArgumentParser(description="GitOps CLI")
+    subparsers = parser.add_subparsers(title="commands")
+
+    deploy_p = subparsers.add_parser("deploy", help="Trigger a new deployment by changing YAML values")
+    deploy_p.set_defaults(command="deploy")
+    deploy_p.add_argument("-r", "--repo", help="the repository URL", required=True)
+    deploy_p.add_argument("-f", "--file", help="the YAML file path", required=True)
+    deploy_p.add_argument(
+        "-v", "--values", help="YAML string with key-value pairs to write", type=yaml.safe_load, required=True,
+    )
+
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    args = parser.parse_args()
+
+    print(args)
 
 
 if __name__ == "__main__":
