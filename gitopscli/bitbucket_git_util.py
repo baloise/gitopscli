d6f43f623475c7c96b2f254d41dc8583459af49d
Christian Siegel <christian.siegel@gmx.net>
2020-01-20 21:19:17 +0100

    Refactor GitUtil to also be responsible for API access (#8)

---
+++ gitopscli/bitbucket_git_util.py
@@ -0,0 +1,53 @@
+import sys
+from atlassian import Bitbucket
+from .abstract_git_util import AbstractGitUtil
+
+
+class BitBucketGitUtil(AbstractGitUtil):
+    def __init__(self, tmp_dir, git_provider_url, organisation, repository_name, username, password):
+        super().__init__(tmp_dir, username, password)
+        self._git_provider_url = git_provider_url
+        self._organisation = organisation
+        self._repository_name = repository_name
+        self._bitbucket = Bitbucket(self._git_provider_url, self._username, self._password)
+
+    def get_clone_url(self):
+        repo = self._bitbucket.get_repo(self._organisation, self._repository_name)
+        if "links" not in repo:
+            print(
+                f"Repository '{self._repository_name}' or organisation '{self._organisation}' does not exist.",
+                file=sys.stderr,
+            )
+            sys.exit(1)
+        for clone_link in repo["links"]["clone"]:
+            if clone_link["name"] == "http":
+                repo_url = clone_link["href"]
+        if not repo_url:
+            print("Couldn't determine repository URL.", file=sys.stderr)
+            sys.exit(1)
+        return repo_url
+
+    def create_pull_request(self, from_branch, to_branch, title, description):
+        pull_request = self._bitbucket.open_pull_request(
+            self._organisation,
+            self._repository_name,
+            self._organisation,
+            self._repository_name,
+            from_branch,
+            to_branch,
+            title,
+            description,
+        )
+        if "errors" in pull_request:
+            for error in pull_request["errors"]:
+                print(error["message"], file=sys.stderr)
+            sys.exit(1)
+        return pull_request
+
+    def get_pull_request_url(self, pull_request):
+        return pull_request["links"]["self"][0]["href"]
+
+    def merge_pull_request(self, pull_request):
+        self._bitbucket.merge_pull_request(
+            self._organisation, self._repository_name, pull_request["id"], pull_request["version"]
+        )
