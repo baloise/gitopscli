465a1687d4a0a411fa4b9bf1a90e11b6f175be54
Christian Siegel <christian.siegel@gmx.net>
2020-02-08 10:04:48 +0100

    fix setup.py packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name = 'gitopscli',
     version = '0.1.0',
-    packages = ['gitopscli'],
+    packages = ['gitopscli', 'gitopscli.commands', 'gitopscli.git', 'gitopscli.yaml'],
     entry_points = {
         'console_scripts': [
             'gitopscli = gitopscli.__main__:main'
