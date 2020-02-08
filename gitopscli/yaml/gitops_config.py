ecc0cf9bf78b7688ec7f8fc5c35aa753b2ef181e
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2020-02-08 22:14:33 +0100

    Fix gitopsconfig paring

--- gitopscli/yaml/gitops_config.py
+++ gitopscli/yaml/gitops_config.py
@@ -8,20 +8,20 @@ class GitOpsConfig:
 
     @property
     def application_name(self):
-        return self._data.get("deploymentConfig.applicationName")
+        return self._data["deploymentConfig"]["applicationName"]
 
     @property
     def team_config_org(self):
-        return self._data.get("deploymentConfig.org")
+        return self._data["deploymentConfig"]["org"]
 
     @property
     def team_config_repo(self):
-        return self._data.get("deployment-config.repository")
+        return self._data["deployment-config"]["repository"]
 
     @property
     def route_host(self):
-        return self._data.get("previewConfig.route.host.template")
+        return self._data["previewConfig"]["route"]["host"]["template"]
 
     @property
     def replacements(self):
-        return self._data.get("previewConfig.replace", [])
+        return self._data["previewConfig"]["replace"]
