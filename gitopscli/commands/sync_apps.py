a3ac7c8f4187624629f9233a03039b29ee9e2326
Christian Siegel <christian.siegel@baloise.ch>
2020-03-16 15:18:28 +0100

    Improve logs and error handling for sync-apps

--- gitopscli/commands/sync_apps.py
+++ gitopscli/commands/sync_apps.py
@@ -63,41 +63,67 @@ def sync_apps_command(
 
 
 def __sync_apps(apps_git, root_git):
-    repo_apps = __get_repo_apps(apps_git)
-    logging.info("Found %s app(s) in %s: %s", len(repo_apps), apps_git.get_clone_url(), ", ".join(repo_apps))
+    logging.info("Apps repository: %s", apps_git.get_clone_url())
+    logging.info("Root repository: %s", root_git.get_clone_url())
 
-    apps_config_file, app_file_name, apps_from_other_repos = __find_apps_config_from_repo(apps_git, root_git)
+    repo_apps = __get_repo_apps(apps_git)
+    logging.info("Found %s app(s) in apps repository: %s", len(repo_apps), ", ".join(repo_apps))
+
+    repo_apps.add("test-app")
+
+    logging.info("Searching apps repository in root repository's 'apps/' directory...")
+    apps_config_file, apps_config_file_name, current_repo_apps, apps_from_other_repos = __find_apps_config_from_repo(
+        apps_git, root_git
+    )
+    if apps_config_file is None:
+        raise GitOpsException(f"Could't find config file for apps repository in root repository's 'apps/' directory")
+
+    if current_repo_apps == repo_apps:
+        logging.info("Root repository already up-to-date. I'm done here.")
+        return
+
     __check_if_app_already_exists(repo_apps, apps_from_other_repos)
+
+    logging.info("Sync applications in root repository's %s.", apps_config_file_name)
     merge_yaml_element(apps_config_file, "applications", dict.fromkeys(repo_apps, {}), True)
-    __commit_and_push(apps_git, root_git, app_file_name)
+    __commit_and_push(apps_git, root_git, apps_config_file_name)
 
 
 def __find_apps_config_from_repo(apps_git, root_git):
-    logging.info("Searching for %s in apps/", apps_git.get_clone_url())
     yaml = YAML()
-    # List for all entries in .applications from each config repository
-    apps_from_other_repos = []
+    apps_from_other_repos = []  # List for all entries in .applications from each config repository
     found_app_config_file = None
     found_app_config_file_name = None
-    app_file_entries = __get_bootstrap_entries(root_git)
-    for app_file in app_file_entries:
-        app_file_name = "apps/" + app_file["name"] + ".yaml"
-        logging.info("Analyzing %s", app_file_name)
+    found_app_config_apps = set()
+    bootstrap_entries = __get_bootstrap_entries(root_git)
+    for bootstrap_entry in bootstrap_entries:
+        if "name" not in bootstrap_entry:
+            raise GitOpsException("Every bootstrap entry must have a 'name' property.")
+        app_file_name = "apps/" + bootstrap_entry["name"] + ".yaml"
+        logging.info("Analyzing %s in root repository", app_file_name)
         app_config_file = root_git.get_full_file_path(app_file_name)
-        with open(app_config_file, "r") as stream:
-            app_config_content = yaml.load(stream)
+        try:
+            with open(app_config_file, "r") as stream:
+                app_config_content = yaml.load(stream)
+        except FileNotFoundError as ex:
+            raise GitOpsException(f"File '{app_file_name}' not found in root repository.") from ex
+        if "repository" not in app_config_content:
+            raise GitOpsException(f"Cannot find key 'repository' in '{app_file_name}'")
         if app_config_content["repository"] == apps_git.get_clone_url():
-            logging.info("Found repository in %s", app_file_name)
+            logging.info("Found apps repository in %s", app_file_name)
             found_app_config_file = app_config_file
             found_app_config_file_name = app_file_name
+            found_app_config_apps = __get_applications_from_app_config(app_config_content)
         else:
-            if "applications" in app_config_content and app_config_content["applications"] is not None:
-                apps_from_other_repos += app_config_content["applications"].keys()
-    if found_app_config_file is None:
-        raise GitOpsException(
-            f"Could't find config file with .repository={apps_git.get_clone_url()} in apps/ directory"
-        )
-    return found_app_config_file, found_app_config_file_name, apps_from_other_repos
+            apps_from_other_repos += __get_applications_from_app_config(app_config_content)
+    return found_app_config_file, found_app_config_file_name, found_app_config_apps, apps_from_other_repos
+
+
+def __get_applications_from_app_config(app_config):
+    apps = []
+    if "applications" in app_config and app_config["applications"] is not None:
+        apps += app_config["applications"].keys()
+    return set(apps)
 
 
 def __commit_and_push(apps_git, root_git, app_file_name):
@@ -109,9 +135,14 @@ def __commit_and_push(apps_git, root_git, app_file_name):
 def __get_bootstrap_entries(root_git):
     root_git.checkout("master")
     bootstrap_values_file = root_git.get_full_file_path("bootstrap/values.yaml")
-    with open(bootstrap_values_file, "r") as stream:
-        bootstrap = YAML().load(stream)
-    return bootstrap["bootstrap"]
+    try:
+        with open(bootstrap_values_file, "r") as stream:
+            bootstrap_yaml = YAML().load(stream)
+    except FileNotFoundError as ex:
+        raise GitOpsException("File 'bootstrap/values.yaml' not found in root repository.") from ex
+    if "bootstrap" not in bootstrap_yaml:
+        raise GitOpsException("Cannot find key 'bootstrap' in 'bootstrap/values.yaml'")
+    return bootstrap_yaml["bootstrap"]
 
 
 def __get_repo_apps(apps_git):
@@ -127,4 +158,4 @@ def __get_repo_apps(apps_git):
 def __check_if_app_already_exists(apps_dirs, apps_from_other_repos):
     for app_key in apps_dirs:
         if app_key in apps_from_other_repos:
-            raise GitOpsException(f"application: {app_key} already exists in a different repository")
+            raise GitOpsException(f"application '{app_key}' already exists in a different repository")
