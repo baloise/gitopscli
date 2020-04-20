0f8b3e912a4e6a397a386209760c99f201e5f822
Christian Siegel <christian.siegel@baloise.ch>
2020-04-20 21:06:33 +0200

    chore(ci): use semantic-release for automatic releases

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="gitopscli",
-    version="3.0.0",
+    version="dev",
     packages=find_packages(),
     entry_points={"console_scripts": ["gitopscli = gitopscli.__main__:main"]},
     setup_requires=["wheel"],
