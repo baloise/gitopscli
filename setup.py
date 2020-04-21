feb530babecb00c0d35cf72d8f64e0c400dcc378
Christian Siegel <christian.siegel@baloise.ch>
2020-04-21 07:20:21 +0200

    release 3.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="gitopscli",
-    version="3.0.1",
+    version="3.1.0",
     packages=find_packages(),
     entry_points={"console_scripts": ["gitopscli = gitopscli.__main__:main"]},
     setup_requires=["wheel"],
