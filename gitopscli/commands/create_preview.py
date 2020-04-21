62e81cfb287a52d35804135d465913e81393fd6f
Christian Siegel <christian.siegel@baloise.ch>
2020-04-21 22:22:33 +0200

    fix(create-preview): improve commit messages and PR comments

--- gitopscli/commands/create_preview.py
+++ gitopscli/commands/create_preview.py
@@ -112,16 +112,16 @@ def create_preview_command(
         if not value_replaced:
             __no_deployment_needed(apps_git, new_image_tag, parent_id, pr_id)
             return
-        root_git.commit(f"Upated preview environment for {gitops_config.application_name} and branch {pr_branch}.")
+        root_git.commit(f"Update preview environment for '{gitops_config.application_name}' and branch '{pr_branch}'.")
         root_git.push(config_branch)
         logging.info("Pushed branch %s", config_branch)
         pr_comment_text = f"""
-New Preview Environment for {gitops_config.application_name} and branch {pr_branch} created successfully. Access it here: 
+New preview environment for `{gitops_config.application_name}` and branch `{pr_branch}` created successfully. Access it here:
 https://{route_host}
 """
         if branch_preview_env_already_exist:
             pr_comment_text = f"""
-Preview Environment for {gitops_config.application_name} and branch {pr_branch} updated successfully. Access it here: 
+Preview environment for `{gitops_config.application_name}` and branch `{pr_branch}` updated successfully. Access it here:
 https://{route_host}
 """
         logging.info("Creating PullRequest comment for pr with id %s and content: %s", pr_id, pr_comment_text)
@@ -170,7 +170,7 @@ def __replace_value(
 def __no_deployment_needed(apps_git, new_image_tag, parent_id, pr_id):
     logging.info("The image tag %s has already been deployed. Doing nothing.", new_image_tag)
     pr_comment_text = f"""
-The version {new_image_tag} has already been deployed. Nothing to do here.
+The version `{new_image_tag}` has already been deployed. Nothing to do here.
 """
     logging.info("Creating PullRequest comment for pr with id %s and content: %s", pr_id, pr_comment_text)
     apps_git.add_pull_request_comment(pr_id, pr_comment_text, parent_id)
@@ -189,7 +189,7 @@ def __create_new_preview_env(
             update_yaml_file(root_git.get_full_file_path(chart_file_path), "name", new_preview_folder_name)
         except KeyError as ex:
             raise GitOpsException(f"Key 'name' not found in '{chart_file_path}'") from ex
-    root_git.commit(f"Create new preview env for application: {app_name} and branch: {pr_branch}")
+    root_git.commit(f"Create new preview environment for '{app_name}' and branch '{pr_branch}'.")
 
 
 def __create_pullrequest(branch, gitops_config, root_git):
