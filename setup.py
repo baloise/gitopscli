74e5d60178b745c60f2bbf3481099c01af793750
Christian Siegel <christian.siegel@baloise.ch>
2020-04-21 17:28:53 +0200

    test: assert 'UNRELEASED' as version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="gitopscli",
-    version="dev",
+    version="UNRELEASED",
     packages=find_packages(),
     entry_points={"console_scripts": ["gitopscli = gitopscli.__main__:main"]},
     setup_requires=["wheel"],
--- tests/commands/test_version.py
+++ tests/commands/test_version.py
@@ -1,4 +1,3 @@
-import re
 import sys
 import unittest
 from contextlib import contextmanager
@@ -23,7 +22,7 @@ class VersionCommandTest(unittest.TestCase):
     def test_output(self):
         with captured_output() as stdout:
             version_command("version")
-        assert re.match(r"^GitOps CLI version \d+\.\d+\.\d+\n$", stdout.getvalue())
+        self.assertEqual("GitOps CLI version UNRELEASED\n", stdout.getvalue())
 
     def test_wrong_command_arg(self):
         with pytest.raises(AssertionError):
