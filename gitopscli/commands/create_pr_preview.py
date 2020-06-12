daed299d260bc43460186a9151324e4aa4ead254
Marco Zirkenbach <m.zirkenbach@gmx.de>
2020-06-12 15:00:29 +0200

    pr feedback

--- gitopscli/commands/create_pr_preview.py
+++ gitopscli/commands/create_pr_preview.py
@@ -56,7 +56,7 @@ def create_pr_preview_command(
             git_provider,
             git_provider_url,
             git_hash,
-            hashlib.sha256(pr_branch.encode("utf-8")).hexdigest()[:8],
+            pr_branch,
             create_deployment_already_up_to_date_callback(parent_id, pr_id),
             create_deployment_exist_callback(parent_id, pr_id, pr_branch),
             create_deployment_new_callback(parent_id, pr_id, pr_branch),
@@ -69,9 +69,7 @@ def create_pr_preview_command(
 def create_deployment_already_up_to_date_callback(parent_id, pr_id):
     def deployment_already_up_to_date_callback(apps_git, new_image_tag):
         logging.info("The image tag %s has already been deployed. Doing nothing.", new_image_tag)
-        pr_comment_text = f"""
-        The version `{new_image_tag}` has already been deployed. Nothing to do here.
-        """
+        pr_comment_text = f"The version `{new_image_tag}` has already been deployed. Nothing to do here."
         logging.info("Creating PullRequest comment for pr with id %s and content: %s", pr_id, pr_comment_text)
         apps_git.add_pull_request_comment(pr_id, pr_comment_text, parent_id)
 
@@ -80,10 +78,9 @@ def create_deployment_already_up_to_date_callback(parent_id, pr_id):
 
 def create_deployment_new_callback(parent_id, pr_id, pr_branch):
     def deployment_new_callback(apps_git, gitops_config, route_host):
-        pr_comment_text = f"""
-        New preview environment for `{gitops_config.application_name}` and branch `{pr_branch}` created successfully. Access it here:
-        https://{route_host}
-        """
+        pr_comment_text = f"""\
+New preview environment for `{gitops_config.application_name}` and branch `{pr_branch}` created successfully. Access it here:
+https://{route_host}"""
         logging.info("Creating PullRequest comment for pr with id %s and content: %s", pr_id, pr_comment_text)
         apps_git.add_pull_request_comment(pr_id, pr_comment_text, parent_id)
 
@@ -92,10 +89,9 @@ def create_deployment_new_callback(parent_id, pr_id, pr_branch):
 
 def create_deployment_exist_callback(parent_id, pr_id, pr_branch):
     def deployment_exist_callback(apps_git, gitops_config, route_host):
-        pr_comment_text = f"""
-        Preview environment for `{gitops_config.application_name}` and branch `{pr_branch}` updated successfully. Access it here:
-        https://{route_host}
-        """
+        pr_comment_text = f"""\
+Preview environment for `{gitops_config.application_name}` and branch `{pr_branch}` updated successfully. Access it here:
+https://{route_host}"""
         logging.info("Creating PullRequest comment for pr with id %s and content: %s", pr_id, pr_comment_text)
         apps_git.add_pull_request_comment(pr_id, pr_comment_text, parent_id)
 
@@ -104,9 +100,8 @@ def create_deployment_exist_callback(parent_id, pr_id, pr_branch):
 
 def __create_pullrequest(branch, gitops_config, root_git):
     title = "Updated preview environment for " + gitops_config.application_name
-    description = f"""
-This Pull Request is automatically created through [gitopscli](https://github.com/baloise/gitopscli).
-"""
+    description = f"""\
+This Pull Request is automatically created through [gitopscli](https://github.com/baloise/gitopscli)."""
     pull_request = root_git.create_pull_request(branch, "master", title, description)
     logging.info("Pull request created: %s", {root_git.get_pull_request_url(pull_request)})
     return pull_request
