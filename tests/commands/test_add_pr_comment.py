e08d70257fc5c30213611b3b95262e6f569d6bc8
Christian Siegel <christian.siegel@baloise.ch>
2020-11-19 14:20:48 +0100

    test(add-pr-comment): fix test after git refactoring

--- tests/commands/test_add_pr_comment.py
+++ tests/commands/test_add_pr_comment.py
@@ -1,12 +1,14 @@
 import unittest
-from uuid import UUID
 from unittest.mock import patch, MagicMock, Mock, call
-import pytest
-from gitopscli.gitops_exception import GitOpsException
+from gitopscli.git import GitApiConfig
 from gitopscli.commands.add_pr_comment import pr_comment_command
 
 
 class AddPrCommentCommandTest(unittest.TestCase):
+    _expected_github_api_config = GitApiConfig(
+        username="USERNAME", password="PASSWORD", git_provider="github", git_provider_url=None,
+    )
+
     def setUp(self):
         def add_patch(target):
             patcher = patch(target)
@@ -15,17 +17,17 @@ class AddPrCommentCommandTest(unittest.TestCase):
 
         # Monkey patch all external functions the command is using:
         self.logging_mock = add_patch("gitopscli.commands.add_pr_comment.logging")
-        self.create_git_mock = add_patch("gitopscli.commands.add_pr_comment.create_git")
-        self.git_util_mock = MagicMock()
+        self.git_repo_api_factory_mock = add_patch("gitopscli.commands.add_pr_comment.GitRepoApiFactory")
+        self.git_repo_api_mock = MagicMock()
 
         # Attach all mocks to a single mock manager
         self.mock_manager = Mock()
-        self.mock_manager.attach_mock(self.create_git_mock, "create_git")
-        self.mock_manager.attach_mock(self.git_util_mock, "git_util")
+        self.mock_manager.attach_mock(self.git_repo_api_factory_mock, "GitRepoApiFactory")
+        self.mock_manager.attach_mock(self.git_repo_api_mock, "GitRepoApi")
         self.mock_manager.attach_mock(self.logging_mock, "logging")
 
         # Define some common default return values
-        self.create_git_mock.return_value = self.git_util_mock
+        self.git_repo_api_factory_mock.create.return_value = self.git_repo_api_mock
 
     def test_with_parent_id(self):
         pr_comment_command(
@@ -42,14 +44,14 @@ class AddPrCommentCommandTest(unittest.TestCase):
         )
 
         assert self.mock_manager.mock_calls == [
-            call.create_git("USERNAME", "PASSWORD", None, None, "ORGA", "REPO", "github", None, None),
+            call.GitRepoApiFactory.create(self._expected_github_api_config, "ORGA", "REPO"),
             call.logging.info(
                 "Creating comment for PR %s as reply to comment %s with content: %s",
                 "<PR ID>",
                 "<PARENT ID>",
                 "Hello World!",
             ),
-            call.git_util.add_pull_request_comment("<PR ID>", "Hello World!", "<PARENT ID>"),
+            call.GitRepoApi.add_pull_request_comment("<PR ID>", "Hello World!", "<PARENT ID>"),
         ]
 
     def test_without_parent_id(self):
@@ -67,7 +69,7 @@ class AddPrCommentCommandTest(unittest.TestCase):
         )
 
         assert self.mock_manager.mock_calls == [
-            call.create_git("USERNAME", "PASSWORD", None, None, "ORGA", "REPO", "github", None, None),
+            call.GitRepoApiFactory.create(self._expected_github_api_config, "ORGA", "REPO"),
             call.logging.info("Creating comment for PR %s with content: %s", "<PR ID>", "Hello World!",),
-            call.git_util.add_pull_request_comment("<PR ID>", "Hello World!", None),
+            call.GitRepoApi.add_pull_request_comment("<PR ID>", "Hello World!", None),
         ]
