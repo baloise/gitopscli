ca2f9f7500e6704c7c88882cc3e5dbde5cc18827
Christian Siegel <christian.siegel@baloise.ch>
2020-11-20 08:08:52 +0100

    test(sync-apps): separate mocks for team and root config repo

--- tests/commands/test_sync_apps.py
+++ tests/commands/test_sync_apps.py
@@ -22,14 +22,20 @@ class SyncAppsCommandTest(unittest.TestCase):
         self.yaml_factory_mock = add_patch("gitopscli.commands.sync_apps.YAML")
         self.merge_yaml_element_mock = add_patch("gitopscli.commands.sync_apps.merge_yaml_element")
 
-        self.git_repo_api_mock = MagicMock()
+        self.team_config_git_repo_api_mock = MagicMock()
+        self.root_config_git_repo_api_mock = MagicMock()
+
+        self.team_config_git_repo_mock = MagicMock()
+        self.root_config_git_repo_mock = MagicMock()
+
         self.yaml_mock = MagicMock()
 
         # Attach all mocks to a single mock manager
         self.mock_manager = Mock()
         self.mock_manager.attach_mock(self.git_repo_api_factory_mock, "GitRepoApiFactory")
-        self.mock_manager.attach_mock(self.git_repo_api_mock, "GitRepoApi")
         self.mock_manager.attach_mock(self.git_repo_mock, "GitRepo")
+        self.mock_manager.attach_mock(self.team_config_git_repo_mock, "GitRepo_team")
+        self.mock_manager.attach_mock(self.root_config_git_repo_mock, "GitRepo_root")
         self.mock_manager.attach_mock(self.os_path_exists_mock, "os.path.exists")
         self.mock_manager.attach_mock(self.os_path_isdir_mock, "os.path.isdir")
         self.mock_manager.attach_mock(self.os_listdir_mock, "os.listdir")
@@ -38,23 +44,41 @@ class SyncAppsCommandTest(unittest.TestCase):
         self.mock_manager.attach_mock(self.yaml_mock, "YAML")
         self.mock_manager.attach_mock(self.merge_yaml_element_mock, "merge_yaml_element")
 
-        self.git_repo_api_factory_mock.create.return_value = self.git_repo_api_mock
-        self.yaml_factory_mock.return_value = self.yaml_mock
-        self.git_repo_api_mock.create_pull_request.return_value = GitRepoApi.PullRequestIdAndUrl(
-            "<dummy pr id>", "<url of dummy pr>"
+        # let GitRepoApiFactory.create() return a different GitRepoApi mock depending on provided repo name
+        self.git_repo_api_factory_mock.create.side_effect = lambda config, org, repo: (
+            self.team_config_git_repo_api_mock if repo == "REPO" else self.root_config_git_repo_api_mock
         )
-        self.git_repo_mock.return_value = self.git_repo_mock
-        self.git_repo_mock.__enter__.return_value = self.git_repo_mock
-        self.git_repo_mock.get_full_file_path.side_effect = lambda x: f"/tmp/created-tmp-dir/{x}"
-        self.git_repo_mock.get_clone_url.return_value = "somerepo"
-        self.git_repo_mock.get_author_from_last_commit.return_value = "author"
+
+        # let GitRepo's constructor return a different mock depending on provided GitRepoApi
+        self.git_repo_mock.side_effect = lambda api: (
+            self.team_config_git_repo_mock
+            if api is self.team_config_git_repo_api_mock
+            else self.root_config_git_repo_mock
+        )
+
+        self.team_config_git_repo_mock.__enter__.return_value = self.team_config_git_repo_mock
+        self.team_config_git_repo_mock.get_clone_url.return_value = "https://team.config.repo.git"
+        self.team_config_git_repo_mock.get_author_from_last_commit.return_value = "author"
+
+        self.root_config_git_repo_mock.__enter__.return_value = self.root_config_git_repo_mock
+        self.root_config_git_repo_mock.get_full_file_path.side_effect = lambda x: f"/tmp/created-tmp-dir/{x}"
+        self.root_config_git_repo_mock.get_clone_url.return_value = "https://root.config.repo.git"
+
         self.os_path_exists_mock.return_value = True
         self.os_path_isdir_mock.return_value = True
+
         self.open_mock.return_value = self.open_mock
         self.open_mock.__enter__.return_value = self.open_mock
+
         self.os_listdir_mock.return_value = ["folder"]
-        bootstrap_values_content = {"bootstrap": [{"name": "team-non-prod"}], "repository": "somerepo"}
-        apps_team_content = {"teamName": "team1", "repository": "somerepo"}
+
+        self.yaml_factory_mock.return_value = self.yaml_mock
+
+        bootstrap_values_content = {
+            "bootstrap": [{"name": "team-non-prod"}],
+            "repository": "https://root.config.repo.git",
+        }
+        apps_team_content = {"teamName": "team1", "repository": "https://team.config.repo.git"}
         self.yaml_mock.load.side_effect = [bootstrap_values_content, apps_team_content]
 
     def test_sync_apps_happy_flow(self):
@@ -82,33 +106,33 @@ class SyncAppsCommandTest(unittest.TestCase):
                 "ROOT_ORGA",
                 "ROOT_REPO",
             ),
-            call.GitRepo(self.git_repo_api_mock),
-            call.GitRepo(self.git_repo_api_mock),
-            call.GitRepo.get_clone_url(),
-            call.logging.info("Team config repository: %s", "somerepo"),
-            call.GitRepo.get_clone_url(),
-            call.logging.info("Root config repository: %s", "somerepo"),
-            call.GitRepo.checkout("master"),
-            call.GitRepo.get_full_file_path("."),
+            call.GitRepo(self.team_config_git_repo_api_mock),
+            call.GitRepo(self.root_config_git_repo_api_mock),
+            call.GitRepo_team.get_clone_url(),
+            call.logging.info("Team config repository: %s", "https://team.config.repo.git"),
+            call.GitRepo_root.get_clone_url(),
+            call.logging.info("Root config repository: %s", "https://root.config.repo.git"),
+            call.GitRepo_root.checkout("master"),
+            call.GitRepo_root.get_full_file_path("."),
             call.os.listdir("/tmp/created-tmp-dir/."),
             call.os.path.isdir("/tmp/created-tmp-dir/./folder"),
             call.logging.info("Found %s app(s) in apps repository: %s", 1, "folder"),
             call.logging.info("Searching apps repository in root repository's 'apps/' directory..."),
-            call.GitRepo.checkout("master"),
-            call.GitRepo.get_full_file_path("bootstrap/values.yaml"),
+            call.GitRepo_root.checkout("master"),
+            call.GitRepo_root.get_full_file_path("bootstrap/values.yaml"),
             call.open("/tmp/created-tmp-dir/bootstrap/values.yaml", "r"),
             call.YAML.load(self.open_mock),
             call.logging.info("Analyzing %s in root repository", "apps/team-non-prod.yaml"),
-            call.GitRepo.get_full_file_path("apps/team-non-prod.yaml"),
+            call.GitRepo_root.get_full_file_path("apps/team-non-prod.yaml"),
             call.open("/tmp/created-tmp-dir/apps/team-non-prod.yaml", "r"),
             call.YAML.load(self.open_mock),
-            call.GitRepo.get_clone_url(),
+            call.GitRepo_team.get_clone_url(),
             call.logging.info("Found apps repository in %s", "apps/team-non-prod.yaml"),
             call.logging.info("Sync applications in root repository's %s.", "apps/team-non-prod.yaml"),
             call.merge_yaml_element(
                 "/tmp/created-tmp-dir/apps/team-non-prod.yaml", "applications", {"folder": {}}, True
             ),
-            call.GitRepo.get_author_from_last_commit(),
-            call.GitRepo.commit("GIT_USER", "GIT_EMAIL", "author updated apps/team-non-prod.yaml"),
-            call.GitRepo.push("master"),
+            call.GitRepo_team.get_author_from_last_commit(),
+            call.GitRepo_root.commit("GIT_USER", "GIT_EMAIL", "author updated apps/team-non-prod.yaml"),
+            call.GitRepo_root.push("master"),
         ]
