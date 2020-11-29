3512a3e55ec3c280b6eb4bff3f15ef6b4cd78e3b
Christian Siegel <christian.siegel@baloise.ch>
2020-11-29 13:54:31 +0100

    chore(tooling): exclude abstract methods from coverage report

--- gitopscli/commands/command.py
+++ gitopscli/commands/command.py
@@ -4,4 +4,4 @@ from abc import ABCMeta, abstractmethod
 class Command(metaclass=ABCMeta):
     @abstractmethod
     def execute(self) -> None:
-        raise NotImplementedError
+        ...
--- gitopscli/git/git_repo_api.py
+++ gitopscli/git/git_repo_api.py
@@ -9,38 +9,38 @@ class GitRepoApi(metaclass=ABCMeta):
 
     @abstractmethod
     def get_username(self) -> Optional[str]:
-        raise NotImplementedError
+        ...
 
     @abstractmethod
     def get_password(self) -> Optional[str]:
-        raise NotImplementedError
+        ...
 
     @abstractmethod
     def get_clone_url(self) -> str:
-        raise NotImplementedError
+        ...
 
     @abstractmethod
     def create_pull_request(
         self, from_branch: str, to_branch: str, title: str, description: str
     ) -> "PullRequestIdAndUrl":
-        raise NotImplementedError
+        ...
 
     @abstractmethod
     def merge_pull_request(self, pr_id: int) -> None:
-        raise NotImplementedError
+        ...
 
     @abstractmethod
     def add_pull_request_comment(self, pr_id: int, text: str, parent_id: Optional[int] = None) -> None:
-        raise NotImplementedError
+        ...
 
     @abstractmethod
     def delete_branch(self, branch: str) -> None:
-        raise NotImplementedError
+        ...
 
     @abstractmethod
     def get_branch_head_hash(self, branch: str) -> str:
-        raise NotImplementedError
+        ...
 
     @abstractmethod
     def get_pull_request_branch(self, pr_id: int) -> str:
-        raise NotImplementedError
+        ...
